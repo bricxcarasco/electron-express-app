7fd47f35da079522eadc872980af5627ae32734f
Bricx Carasco <bricxraincarasco21@gmail.com>
2020-06-22 16:28:35 +0800

    Install dotenv and update database credentials

--- scripts/mysqlConnection.js
+++ scripts/mysqlConnection.js
@@ -1,11 +1,15 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const connection = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME
-})
+});
 
-connection.connect()
+connection.connect();
 
 module.exports = connection;
